22654966dd47592e917356fcc7167561902c6255
benjspriggs <benjspriggs@sprico.com>
2014-09-05 17:53:44 -0700

    kk

--- testcookies.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once('lib/init.php');
-Cookie::create(Config::get('remember/cookie_name'), TRUE, Config::get('remember/cookie_expiry'));
-Cookie::destroy(Config::get('remember/cookie_name'));
-//header("Location: home.php");
-?>
